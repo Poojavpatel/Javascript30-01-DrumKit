2bfb40a61286c7104e4d340a14c30f6e36c41479
Pooja patel <poojavpatel71@gmail.com>
2018-07-15 22:40:50 +0530

    audio play on keydown|not working

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+/*jshint esversion: 6 */
 console.log("its working");
 window.addEventListener('keydown',function (e){
-    console.log("no its not");
+    console.log(e.keyCode);
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio);
+
+    //ifthereisnoaudioreturnfromhere
+    if(!audio) return;
+    //ifthereisaudioplayit
+    audio.currentTime=0;
+    audio.play();
+
+
 });
