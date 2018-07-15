52140bb4d96d5446dec69a5622b0cd522ccf73d1
Pooja patel <poojavpatel71@gmail.com>
2018-07-16 02:25:31 +0530

    add lass on keydown|remoee on transitionend

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 console.log("its working");
 window.addEventListener('keydown',function(e){
     console.log(e.keyCode);
+
     const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    //console.log(audio);
+    //console.log(key);
 
     //ifthereisnoaudioreturnfromhere
     if(!audio) return;
     //ifthereisaudioplayit
     audio.currentTime=0;//rewind to the start
     audio.play();
-
-
+    key.classList.add('rockit');
 }); 
+
+const keys=document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', (e) => {
+   // console.log(e);
+    e.target.classList.remove('rockit');
+})
+);
+
